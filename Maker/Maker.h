26d1f290f869d0f62c1309e6cfdf67b73c4c4c7d
WWDC <wwdc@look.local>
2016-04-15 19:26:30 +0530

    Initial Commit

---
+++ Maker/Maker.h
@@ -0,0 +1,19 @@
+//
+//  Maker.h
+//  Maker
+//
+//  Created by WWDC on 15/04/16.
+//  Copyright © 2016 WWDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Maker.
+FOUNDATION_EXPORT double MakerVersionNumber;
+
+//! Project version string for Maker.
+FOUNDATION_EXPORT const unsigned char MakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Maker/PublicHeader.h>
+
+
